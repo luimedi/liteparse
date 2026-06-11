2424dc65a50460576681925417a9b8f88e1c6f72
Luis Medina <6675716+luimedi@users.noreply.github.com>
2026-06-11 20:21:53 +0200

    Initial commit

---
+++ lib/lite_parse.ex
@@ -0,0 +1,18 @@
+defmodule LiteParse do
+  @moduledoc """
+  Documentation for `LiteParse`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LiteParse.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/lite_parse_test.exs
@@ -0,0 +1,8 @@
+defmodule LiteParseTest do
+  use ExUnit.Case
+  doctest LiteParse
+
+  test "greets the world" do
+    assert LiteParse.hello() == :world
+  end
+end
